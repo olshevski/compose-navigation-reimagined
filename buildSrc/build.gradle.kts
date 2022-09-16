a908d1b349eb500f2d09be3fe692e1eba192b1b5
Vitali Olshevski <tech@olshevski.dev>
2022-09-16 19:10:47 +0300

    Fix Hilt compilation issues caused by Android Gradle Plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -16,4 +16,7 @@ java {
 dependencies {
     implementation("com.android.tools.build:gradle:7.3.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+
+    // fix the issue with Hilt down the line: https://github.com/google/dagger/issues/3068
+    implementation("com.squareup:javapoet:1.13.0")
 }
